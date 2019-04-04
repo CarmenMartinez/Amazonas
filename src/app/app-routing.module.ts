9cc0bc4804cbecf151de8e2444649a9e93f821f0
Carmen Martinez <carmen.mtzn@gmail.com>
2019-04-04 12:19:56 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ProductosListaComponent } from './productos/productos-lista/productos-lista.component';
+import { ProductoDetalleComponent } from './productos/producto-detalle/producto-detalle.component';
 
-const routes: Routes = [];
+/*
+En la tarea existen las siguientes rutas:
+1. servidor/home muestra el home
+2. servidor/productos
+  a. servidor/productos lista de artículos y permite añadir al carrito de compra
+  b. servidor/productos/id muestra el detalle
+3. servidor/carrito
+  a. servidor/carrito muestra la lista de productos seleccionados al carrito
+  b. servidor/carrito/id muestra el detalle del artículo (todos los campos)
+*/
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'home', component: HomeComponent },
+  {path: 'productos', component: ProductosComponent, children: [
+    {path: '', component: ProductosListaComponent},
+    {path: ':id', component: ProductoDetalleComponent},
+
+  ]},
+  {path: 'carrito', component: ProductosComponent, children: [
+    {path: '', component: ProductosListaComponent},
+    {path: ':id', component: ProductoDetalleComponent},
+
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/productos/Producto.ts
@@ -0,0 +1,8 @@
+export class Alumno {
+    id: number;
+    nombre: string;
+    marca: string;
+    categoria: string;
+    precio: number;
+    existencia: number;
+}
